17976c55809bf6288746a512f1c5efbe2a1a82c8
SGCMarkus <markusornik@gmail.com>
2023-07-19 14:11:55 +0200

    qcom: fix paths for compiling for inline

--- qcom/opensource/datarmnet/core/rmnet_trace.h
+++ qcom/opensource/datarmnet/core/rmnet_trace.h
@@ -13,7 +13,7 @@
 #define TRACE_INCLUDE_PATH ../../../../../../../src/datarmnet/core
 #endif /* endif LINUX_VERSION_CODE < KERNEL_VERSION(5,5,0) */
 #else
-#define TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/datarmnet/core
+#define TRACE_INCLUDE_PATH ../../../../sm8450-modules/qcom/opensource/datarmnet/core
 #endif
 #define TRACE_INCLUDE_FILE rmnet_trace
 
