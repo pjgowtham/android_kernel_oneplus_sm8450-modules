OnePlus-11-Development/android_kernel_oneplus_sm8550-modules

display-drivers: oplus: Fix header includes
Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

qcom/display-drivers/oplus/oplus_onscreenfingerprint.c
@@ -15,7 +15,7 @@
 #include "sde_encoder_phys.h"
 #include "sde_trace.h"
 #include <linux/msm_drm_notify.h>
-#include "../../../oplus/kernel/touchpanel/oplus_touchscreen_v2/touchpanel_notify/touchpanel_event_notify.h"
+#include "../../../../sm8550/drivers/input/touchscreen/oplus_touchscreen_v2/touchpanel_notify/touchpanel_event_notify.h"
 
 #if defined(CONFIG_PXLW_IRIS)
 #include "dsi_iris_api.h"

pjgowtham/android_kernel_oneplus_sm8450-modules

qcom/display-drivers/oplus/oplus_onscreenfingerprint.c
@@ -14,7 +14,7 @@
 #include "sde_encoder_phys.h"
 #include "sde_trace.h"
 #include <linux/msm_drm_notify.h>
-#include "../../../oplus/kernel/touchpanel/oplus_touchscreen_v2/touchpanel_notify/touchpanel_event_notify.h"
+#include "../../../../sm8450/drivers/input/touchscreen/oplus_touchscreen_v2/touchpanel_notify/touchpanel_event_notify.h"
 #ifdef OPLUS_FEATURE_DISPLAY
 #include "oplus_display_temperature.h"
 #include "oplus_display_panel_common.h"