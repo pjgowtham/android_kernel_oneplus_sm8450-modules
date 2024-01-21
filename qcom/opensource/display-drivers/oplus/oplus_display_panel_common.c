a3f9f6e5a4ff206637d8e946e1a71ba094475fc6
Machad3x <machad3x@gmail.com>
2024-01-21 14:21:48 -0400

    treewide: oplus: Address clang-17 build warnings

--- qcom/opensource/display-drivers/oplus/oplus_display_panel_common.c
+++ qcom/opensource/display-drivers/oplus/oplus_display_panel_common.c
@@ -3199,7 +3199,7 @@ int oplus_display_update_dbv(struct dsi_panel *panel)
 }
 /* end for pwm onepulse feature */
 
-int oplus_display_panel_set_demua()
+int oplus_display_panel_set_demua(void)
 {
 	u32 bl_lvl = 0;
 	int rc = 0;
